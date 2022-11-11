4c75b098d4f8ac626b3f5928256e64c9cea857dc
BonhyeonGu <tayasriel@gmail.com>
2022-11-11 12:19:35 +0900

    파일 삭제 구현

--- view/file.py
+++ view/file.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, session, redirect, url_for, render_template, jsonify, request
 from werkzeug.utils import secure_filename
 from pymongo import MongoClient
+from os import listdir, remove
 #------------------------------------------------------------------------
 from secret.secret import mongo_dbid, mongo_dbpw, mongo_dbaddr, mongo_dbport
 #------------------------------------------------------------------------
@@ -11,6 +12,7 @@ db = client['ds']
 col_uf = db['uploaded_file']
 col_f4g = db['file4group']
 col_s4g = db['schedule4group']
+LOCATION = './files/'
 #------------------------------------------------------------------------
 #!!! 입 또는 출력이 리스트인 경우는 사이즈가 1인 리스트의 경우를 포함함
 #앞글자가 a인 것은 출력이 중요하지 않음, j는 출력이 중요함
@@ -21,30 +23,29 @@ def fileIndex():
 
 #현재 서버에 업로드된 파일들을 출력
 #입력없음 출력리스트
-@file.route("/jFilesOut2S")
+@file.route("/jFilesOut2S", methods=['POST'])
 def jFilesOut2S():
-	print(col_uf.find())
-	return render_template('file_setting.html', names=names, dates_edit=dates_edit, use=u.outDirByte(location))
+	ret = listdir(LOCATION)
+	j = {"ret" : ret}
+	return jsonify(j)
 
-#서버에 파일 업로드
-#입력없음 출력없음
+#서버에 파일 업로드, 얘는 json아니고 리플래시를 요구함
+#입력파일 출력없음
 @file.route("/upload", methods=['POST'])
 def upload():
 	inp_file = request.files['file']
-	inp_file.save('./files/' + secure_filename(inp_file.filename))
-
+	inp_file.save(LOCATION + secure_filename(inp_file.filename))
 	return render_template('fileIndex.html')
-
 
 #서버에 업로드된 파일을 삭제 (GID가 사용중인지 확인? 또는 강제로?)
 #입력{gid, 파일리스트} 출력없음
-@file.route("/aDeleteFiles")
+@file.route("/aDeleteFiles", methods=['POST'])
 def aDeleteFiles():
-	if not 'no' in session:
-		return redirect(url_for('login'))
-	location = "files" + session['id']
-	names, dates_edit = u.outDirList(location)
-	return render_template('file_setting.html', names=names, dates_edit=dates_edit, use=u.outDirByte(location))
+	params = request.get_json()
+	fileName = params['fileName']
+	print(fileName)
+	remove(LOCATION + fileName)
+	return jsonify({"msg" : 'clear'})
 
 #해당 GID가 '사용하고 있는' 파일들을 출력
 #입력gid 출력리스트
---
+++ view/static/js/fileIndex.js
@@ -0,0 +1,37 @@
+function reloadFileList(){
+    $("#fileList").html('');
+    $.ajax({
+        url: "/file/jFilesOut2S",
+        type: "POST",
+        async: false,
+        dataType: "json",
+        contentType: "application/json",
+        success: function(res){
+            for(let fileListLine of res.ret){
+                let code = `<div class="fileListLine">${fileListLine}</div>`;
+                $("#fileList").append(code);
+            }
+        }
+    });
+}
+
+function deleteFile(){
+    $("#deleteBtn").on("click", function(){
+        $.ajax({
+            url: "/file/aDeleteFiles",
+            type: "POST",
+            async: true,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({fileName : $("#deleteInp").val()}),
+            success: function(res){
+            }
+        });
+        reloadFileList();
+    });
+}
+
+$(document).ready(function(){
+    reloadFileList();
+    deleteFile();
+});
